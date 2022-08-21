19ddeb833819909f3254d1fd63c552f2ac13d7d3
Kevin <kevinlarocquepro@gmail.com>
2022-08-20 20:56:05 -0400

    added module to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,13 +5,13 @@ Vue.use(Vuex)
 
 import SkillModule from "./modules/SkillModule";
 import LangModule from './modules/LangModule';
-import LoginModule from './modules/LoginModule';
 import ExpModule from './modules/ExpModule';
+import ContactModule from './modules/ContactModule';
 
 const store = new Vuex.Store({});
 export default store;
 
 const skillModule = new SkillModule({store: store, name: "SkillModule"});
 const langModule = new LangModule({store: store, name: "LangModule"});
-const loginModule = new LoginModule({store: store, name: "LoginModule"});
 const expModule = new ExpModule({store: store, name: "ExpModule"});
+const contactModule = new ContactModule({store: store, name: "ContactModule"});
